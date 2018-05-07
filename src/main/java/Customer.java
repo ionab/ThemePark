03dc8cb77def8fe3613c424b4803273a7c7f92b7
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-07 16:54:10 +0100

    Abstract status removed from customer class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,3 +1,27 @@
-public abstract class Customer {
-    private
+public class Customer {
+    private int age;
+    private int height;
+    private double money;
+
+    public Customer(int age, int height, double money) {
+        this.age = age;
+        this.height = height;
+        this.money = money;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
 }
